5d27122996418479339ba8c1ff66dae477cacd95
nilesh-singh-2304 <shyaam2304@gmail.com>
2025-02-15 03:23:00 +0530

    new

--- pages/member/quiz/index.tsx
+++ pages/member/quiz/index.tsx
@@ -8,29 +8,13 @@ const index = () => {
       <MemSidebar />
 
       <div className="p-4 sm:ml-64">
-        <div className="flex flex-col mt-20">
-          <div className="card lg:card-side bg-base-100 shadow-xl">
-            <figure>
-              <img
-                src="https://img.daisyui.com/images/stock/photo-1494232410401-ad00d5433cfa.webp"
-                alt="Album"
-              />
-            </figure>
-            <div className="card-body">
-              <h2 className="card-title">AI Topic Wise Quiz!</h2>
-              <p>Click the button to listen on Spotiwhy app.</p>
-              <div className="card-actions justify-end">
-                <Link href="/member/quiz/topicquiz"><button className="btn btn-primary">Take Quiz</button></Link>
-              </div>
-            </div>
-          </div>
-        </div>
 
-        <div className="flex flex-col mt-20">
+      <div className="flex flex-col mt-20">
           <div className="card lg:card-side bg-base-100 shadow-xl">
             <figure>
               <img
-                src="https://img.daisyui.com/images/stock/photo-1494232410401-ad00d5433cfa.webp"
+                src="https://canopylab.io/wp-content/uploads/2023/01/Blog-Creating-multiple-choice-quizzes-with-the-CanopyLAB-Quiz-engine.jpg"
+                className="h-96"
                 alt="Album"
               />
             </figure>
@@ -39,6 +23,26 @@ const index = () => {
               <p>Click the button to listen on Spotiwhy app.</p>
               <div className="card-actions justify-end">
                 <Link href="/member/quiz/pdfquiz"><button className="btn btn-primary">Take Quiz</button></Link>
+              </div>
+            </div>
+          </div>
+        </div>
+
+
+        <div className="flex flex-col mt-20">
+          <div className="card lg:card-side bg-base-100 shadow-xl">
+            <figure>
+              <img
+                src="https://cdn.analyticsvidhya.com/wp-content/uploads/2024/01/cover-page--scaled.jpg"
+                className="h-96"
+                alt="Album"
+              />
+            </figure>
+            <div className="card-body">
+              <h2 className="card-title">AI Topic Wise Quiz!</h2>
+              <p>Click the button to listen on Spotiwhy app.</p>
+              <div className="card-actions justify-end">
+                <Link href="/member/quiz/topicquiz"><button className="btn btn-primary">Take Quiz</button></Link>
               </div>
             </div>
           </div>
--- pages/member/yt.tsx
+++ pages/member/yt.tsx
@@ -57,7 +57,7 @@ const Yt = (yt) => {
             <div className="max-w-6xl max-lg:max-w-3xl max-sm:max-w-sm mx-auto">
               <div className="max-w-md mx-auto">
                 <h2 className="text-3xl font-extrabold text-gray-800 mb-12 text-center leading-10">
-                  Stay updated with the latest blog posts.
+                  Enjoy Ad-Free youtube support with power of AI
                 </h2>
               </div>
               <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 max-sm:gap-8">
