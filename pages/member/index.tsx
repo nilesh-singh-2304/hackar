5565fbf69759f8d948373fb0ed8f404aca5fd93b
nilesh-singh-2304 <shyaam2304@gmail.com>
2025-04-25 16:18:53 +0530

    new

--- pages/member/index.tsx
+++ pages/member/index.tsx
@@ -5,7 +5,7 @@ import MemSidebar from '@/components/memSidebar'
 import { log } from 'console';
 import Link from 'next/link';
 const Index =({blogs}) => {
-   console.log(blogs)
+   console.log(blogs[0].images[0].url);
   return (
     <div>
       <MemSidebar />
@@ -21,16 +21,16 @@ const Index =({blogs}) => {
       <div className="container mx-auto">
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
          {blogs.map((blog) => (
-            <div key={blog.title} className="bg-white rounded-lg overflow-hidden shadow-lg">
-            <img src={blog.newsImgSrc} alt="Blog Post 1" className="w-full h-52 object-top" />
+            <div key={blog.bio} className="bg-white rounded-lg overflow-hidden shadow-lg">
+            <img src={blog.images[0].url} alt="Blog Post 1" className="w-full h-52 object-top" />
             <div className="p-6">
                <div className='flex justify-start gap-5 mb-2'>
                   <span><b>Date:</b> {blog.newsDatePublished}</span>
                   <span><b>Read:</b> {blog.newsTimetoRead}</span>
                </div>
-              <h3 className="text-xl font-bold text-gray-800 mb-2">{blog.newsTitle}</h3>
-              <p className="text-gray-600 text-sm">{blog.newsSubHeadline}</p>
-              <Link href={blog.newsUrl} className="mt-4 inline-block text-blue-600 text-sm hover:underline">Read More</Link>
+              <h3 className="text-xl font-bold text-gray-800 mb-2">{blog.title}</h3>
+              <p className="text-gray-600 text-sm">{blog.bio}</p>
+              <Link href={blog.webUrl} className="mt-4 inline-block text-blue-600 text-sm hover:underline">Read More</Link>
             </div>
           </div>
          ))} 
@@ -46,18 +46,19 @@ const Index =({blogs}) => {
 }
 
 export async function getServerSideProps() {
-   const options = {
-      method: 'GET',
-      url: 'https://ieee-spectrum-api.p.rapidapi.com/api',
-      headers: {
-        'x-rapidapi-key': 'efdbb399d6msh78a4b7750642cc8p197564jsnff76aecec8b1',
-        'x-rapidapi-host': 'ieee-spectrum-api.p.rapidapi.com'
-      }
-    };
-    
-    
-        const response = await axios.request(options);
-       let blogs = response.data;
+  const options = {
+    method: 'GET',
+    url: 'https://ai10.p.rapidapi.com/people/machine-learning/page/1/',
+    headers: {
+      'x-rapidapi-key': 'efdbb399d6msh78a4b7750642cc8p197564jsnff76aecec8b1',
+      'x-rapidapi-host': 'ai10.p.rapidapi.com'
+    }
+  };
+  
+  
+    const response = await axios.request(options);
+    console.log(response.data.value);
+       let blogs = response.data.value;
 
        return{
          props:{blogs: JSON.parse(JSON.stringify(blogs))} // will be passed to page components as props
