1f9bb4769626fd43bcc75057a56536cced03e260
nilesh-singh-2304 <shyaam2304@gmail.com>
2025-05-21 14:52:54 +0530

    new

--- pages/member/class/index.tsx
+++ pages/member/class/index.tsx
@@ -105,7 +105,7 @@ const Class = () => {
   </div>
 </section>
 
-<script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/datepicker.min.js"></script>
+{/* <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/datepicker.min.js"></script> */}
     </div>
   )
 }
